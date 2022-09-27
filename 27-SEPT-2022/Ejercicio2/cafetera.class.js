c3423aaa9d5a269dc6f2f994578c2194fe8ddc49
Nagib <nagib.86@gmail.com>
2022-09-27 20:02:44 +0200

    terminado cafetera y creando ventanas emergentes

--- 27-SEPT-2022/Ejercicio2/cafetera.class.js
+++ 27-SEPT-2022/Ejercicio2/cafetera.class.js
@@ -9,29 +9,99 @@ class Cafetera {
     }
 
     servirVaso(){
+        var mensaje;
         if(this.capacidadVaso > this.capacidadCafetera){
-            alert("La cafetera se ha quedado sin agua. POr favor, recárguela.<br>" + 
+            mensaje = "La cafetera se ha quedado sin agua. Por favor, recárguela.\n" + 
+            "No se ha servido el café.";
+            throw mensaje;
+        }
+        else{
+            this.capacidadCafetera -= this.capacidadVaso;
+            alert("Ha servido un vaso de café");
+        }
+        /*if(this.capacidadVaso > this.capacidadCafetera){
+            alert("La cafetera se ha quedado sin agua. Por favor, recárguela.\n" + 
                   "No se ha servido el café.");
         }
         else{
         //this.capacidadCafetera = this.capacidadCafetera - this.capacidadVaso;
         this.capacidadCafetera -= this.capacidadVaso;
         alert("Ha servido un vaso de café");
-        }
+        }*/
     }
 
     servirTaza(){
+        var mensaje;
         if(this.capacidadTaza > this.capacidadCafetera){
-            alert("La cafetera se ha quedado sin agua. POr favor, recárguela.<br>" + 
+            mensaje = "La cafetera se ha quedado sin agua. Por favor, recárguela.\n" + 
+            "No se ha servido el café.";
+            throw mensaje;
+        }
+        else{
+            this.capacidadCafetera -= this.capacidadTaza;
+            alert("Ha servido un vaso de café");
+        }
+        /*if(this.capacidadTaza > this.capacidadCafetera){
+            alert("La cafetera se ha quedado sin agua. Por favor, recárguela.\n" + 
                   "No se ha servido el café.");
         }
         else{
             this.capacidadCafetera -= this.capacidadTaza;
             alert("Ha servido una taza de café");
-        }
+        }*/
     }
 
     recargar(){
         this.capacidadCafetera = this.capacidadMaximaCafetera;
+        alert("Cafetera recargada.")
+    }
+
+    //seters
+    setCapacidadMaximaCafetera(p_capacidadMaximaCafetera){
+        if(isNaN(p_capacidadMaximaCafetera)){
+            alert("Formato incorrecto, debe indicar un número");
+        }
+        else{
+            this.capacidadMaximaCafetera = p_capacidadMaximaCafetera
+        }
+    }
+
+    setCapacidadCafetera(p_capacidadCafetera){
+        if(isNaN(p_capacidadCafetera)){
+            alert("Formato incorrecto, debe indicar un número");
+        }
+        else{
+            this.capacidadCafetera = p_capacidadCafetera
+        }
+    }
+
+    setCapacidadVaso(p_capacidadVaso){
+        if(isNaN(p_capacidadVaso)){
+            alert("Formato incorrecto, debe indicar un número");
+        }
+        else{
+            this.capacidadVaso= p_capacidadVaso
+        }
+    }
+
+    setCapacidadTaza(p_capacidadTaza){
+        if(isNaN(p_capacidadTaza)){
+            alert("Formato incorrecto, debe indicar un número");
+        }
+        else{
+            this.capacidadTaza = p_capacidadTaza
+        }
+    }
+
+    //getters
+    getCapacidadCafetera(){
+        var mensaje;
+        if(this.capacidadCafetera == 0){
+            mensaje = "Cafetera vacía";
+        }
+        else{
+            mensaje = "La cafetera contiene: " + this.capacidadCafetera
+        }
+        return mensaje;
     }
 }
--- 27-SEPT-2022/Ejercicio2/miScript.js
+++ 27-SEPT-2022/Ejercicio2/miScript.js
@@ -1,7 +1,8 @@
-let cafeteraCasa = new Cafetera(1000, 250, 75);
-let cafeteraOficina = new Cafetera(2000, 440, 200);
+//let cafeteraCasa = new Cafetera(1000, 250, 75);
+//let cafeteraOficina = new Cafetera(2000, 440, 200);
 
-let miCafetera = new Cafetera();
+//let miCafetera = new Cafetera();
+let miCafetera = null;
 let opcion;
 
 let menu = "= MENÚ CAFETERA = \n"
@@ -10,27 +11,63 @@ let menu = "= MENÚ CAFETERA = \n"
         +  "2. Servir vaso\n"
         +  "3. Servir taza\n"
         +  "4. Recargar\n"
-        +  "5. Salir\n";
+        +  "5. Mostrar cantidad agua\n"
+        +  "6. Salir\n";
 
 do{
     //alert(menu);
     opcion = prompt(menu);
     switch(opcion){
         case "1":
-            miCafetera.capacidadMaximaCafetera = prompt("Intro. la capacidad máxima de la cafetera: ");
-            miCafetera.capacidadCafetera = miCafetera.capacidadMaximaCafetera;
-            miCafetera.capacidadVaso = prompt("Introduce la capacidad del vaso: ");
-            miCafetera.capacidadTaza = prompt("INtroduce la capacidad de la taza: ");
+            miCafetera = new Cafetera();
+            //miCafetera.capacidadMaximaCafetera = prompt("Intro. la capacidad máxima de la cafetera: ");
+            miCafetera.setCapacidadMaximaCafetera(parseInt( prompt("Intro. la capacidad máxima de la cafetera") ));
+            miCafetera.setCapacidadCafetera( miCafetera.capacidadMaximaCafetera);
+            //miCafetera.capacidadCafetera = miCafetera.capacidadMaximaCafetera;
+            //miCafetera.capacidadVaso = prompt("Introduce la capacidad del vaso: ");
+            miCafetera.setCapacidadVaso(parseInt( prompt("Introduce la capacidad del vaso: ") ));
+            //miCafetera.capacidadTaza = prompt("Introduce la capacidad de la taza: ");
+            miCafetera.setCapacidadTaza(parseInt( prompt("Introduce la capacidad de la taza: ") ));
             break;
         case "2":
+            try{
+                if(miCafetera!=null){
+                    miCafetera.servirVaso();
+                }
+                else{
+                    alert("Primero debe crear una cafetera.");
+                }
+                
+            }catch(p_error){
+                alert(p_error);
+            }            
             break;
         case "3":
+            try{
+                if(miCafetera!=null){
+                    miCafetera.servirTaza();
+                }
+                else{
+                    alert("Primero debe crear una cafetera.");
+                }
+            }catch(p_error){
+                alert(p_error);
+            }
             break;
         case "4":
+            if(miCafetera!=null){
+                miCafetera.recargar();
+            }
+            else{
+                alert("Primero debe crear una cafetera.");
+            }
             break;
         case "5":
+            alert( miCafetera.getCapacidadCafetera() );
+            break;
+        case "6":
             break;
         default:
-            alert("Opcion incorrecta")
-;    }
-}while(opcion != 5);
+            alert("Opcion incorrecta");
+    }
+}while(opcion != 6);
